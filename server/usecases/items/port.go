f23fb6de59d9b93f19690ae1fb7fbc8a89d5c5c9
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-07-27 01:17:30 +0900

    modify_usecase

--- server/usecases/items/port.go
+++ server/usecases/items/port.go
@@ -1,21 +1,10 @@
 package items
 
 import (
-	"context"
 	"item_management/server/entities"
 )
 
-type ItemInputPort interface {
-	AddItem(ctx context.Context, item *entities.Item) error
-	GetItems(ctx context.Context) error
-}
-
-type ItemOutputPort interface {
-	OutputItems([]*entities.Item) error
-	OutputError(error) error
-}
-
-type ItemRepository interface {
-	AddItem(ctx context.Context, item *entities.Item) ([]*entities.Item, error)
-	GetItems(ctx context.Context) ([]*entities.Item, error)
+type ItemPort interface {
+	FindItemByID(id int) (entities.Item, error)
+	FindAllItems() (entities.Items, error)
 }
--- server/usecases/users/interactor.go
+++ server/usecases/users/interactor.go
@@ -1,36 +1,19 @@
 package users
 
 import (
-	"context"
 	"item_management/server/entities"
 )
 
 type UserInteractor struct {
-	OutputPort UserOutputPort
-	Repository UserRepository
+	UserPort UserPort
 }
 
-func NewUserInputPort(outputPort UserOutputPort, repository UserRepository) UserInputPort {
-	return &UserInteractor{
-		OutputPort: outputPort,
-		Repository: repository,
-	}
+func (interactor *UserInteractor) FindUserByID(id int) (user entities.User, err error) {
+	user, err = interactor.UserPort.FindUserByID(id)
+	return
 }
 
-func (u *UserInteractor) AddUser(ctx context.Context, user *entities.User) error {
-	users, err := u.Repository.AddUser(ctx, user)
-	if err != nil {
-		return u.OutputPort.OutputError(err)
-	}
-
-	return u.OutputPort.OutputUsers(users)
-}
-
-func (u *UserInteractor) GetUsers(ctx context.Context) error {
-	users, err := u.Repository.GetUsers(ctx)
-	if err != nil {
-		return u.OutputPort.OutputError(err)
-	}
-
-	return u.OutputPort.OutputUsers(users)
+func (interactor *UserInteractor) FindAllUsers() (users entities.Users, err error) {
+	users, err = interactor.UserPort.FindAllUsers()
+	return
 }
